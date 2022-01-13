4343852ff2d38bc9902a9d8930fb297efd6d97db
wonengzudang <huangwanpeng1@gmail.com>
2022-01-13 19:16:09 +0900

    update

--- cfgs/myconfig_10Hz.py
+++ cfgs/myconfig_10Hz.py
@@ -33,10 +33,10 @@ SIM_HOST = "localhost"
 
 DONKEY_GYM_ENV_NAME = "donkey-mountain-track-v0" 
 #GYM_CONF = { "body_style" : "bare", "body_rgb" : (69, 69, 69), "car_name" : "hirohkau_Ahoy!", "font_size" : 18} # body style(donkey|bare|car01) body rgb 0-255
-GYM_CONF = { "body_style" : "donkey", "body_rgb" : (230, 0, 50), "car_name" : "Ahoy!", "font_size" : 18} # body style(donkey|bare|car01) body rgb 0-255
+GYM_CONF = { "body_style" : "donkey", "body_rgb" : (230, 0, 50), "car_name" : "Huang", "font_size" : 18} # body style(donkey|bare|car01) body rgb 0-255
 
 
-GYM_CONF["racer_name"] = "hirohaku"
+GYM_CONF["racer_name"] = "Huang"
 GYM_CONF["country"] = "JP"
 GYM_CONF["bio"] = "HELLO"
 
